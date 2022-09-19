a4f4e5dde1b8afc9ac4db542e58d0ff6f6f55882
WaleYusuf <iyusuf023112@gmail.com>
2022-09-19 07:42:04 +0000

    Update 4 files

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,8 +1,4 @@
-function squareDigit(str){
-    return str.split('').map((elem) => Math.pow(elem, 2)).join('')
-   }
-   console.log(squareDigit('9119'))
-   console.log(squareDigit('289'))
-   console.log(squareDigit('345'))
-   console.log(squareDigit('7772'))
+function squareDigit(str) {
+ 
+ }
   module.exports =squareDigit
